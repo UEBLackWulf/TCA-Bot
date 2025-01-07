8e5eb7f4e29708c63f9974d511e7ebdfb4699387
Alice <168473440+UEBlackWulfGHG@users.noreply.github.com>
2025-01-07 12:48:21 +0100

    Aktualisieren von index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-requier('dotenv')
+requier('dotenv').config
+const { Client, Events, GatewayIntentBits } = require('discord.js);
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once(Events.ClientReady, readyClient => {
+        console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
